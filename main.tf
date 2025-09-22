be87bfeba0f383f040cdad955c262c968d51b8da
ViolinCake <abdoessam044@gmail.com>
2025-09-22 14:16:17 +0300

    added remote backend for statefile with S# and DynamoDb

--- main.tf
+++ main.tf
@@ -1,3 +1,55 @@
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "my-terraform-state-bucket-iti-project" # must be globally unique
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-iti-project"
+    key            = "global/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+
+
+
+
+
+
+
 module "network" {
   source = "./vpc"
   subnet_a = module.cluster1.public_subnet
